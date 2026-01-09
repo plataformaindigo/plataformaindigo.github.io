f25e37ba48ceaebf78e882f55d67dbbfdf6a11fb
plataformaindigo <jorgemartingrassi@gmail.com>
2026-01-09 14:14:36 -0300

    Create datosCliente.js

---
+++ datosCliente.js
@@ -0,0 +1,18 @@
+const CLIENTE = {
+  logo: "data:image/png;base64,.....", // base64 de la imagen
+  nombre: "Clínica Médica Índigo",
+  especialidades: "Especialidades médicas",
+  descripcion: `
+    CLINICA MEDICA INDIGO<br>
+    Clínica virtual con especialistas ficticios, creada con fines de prueba del sistema de emisión de informes médicos de la Plataforma INDIGO.<br><br>
+    Solicite su propia versión personalizada del sistema, utilizando su nombre institucional o profesional, identidad visual, membretes y estructura de informes médicos adaptada a sus prácticas.<br><br>
+    Plataforma INDIGO ofrece una prueba de 30 días, sin compromiso de suscripción, durante la cual podrá generar informes médicos con la estética y estructura de su institución o consultorio.
+  `,
+  MENU: [
+    { nombre: "PSIQUIATRIA", links: [{ text: "Psi. Romina Vázquez", medico: "indigopsivazquez" }] },
+    { nombre: "RAYOS X", links: [{ text: "Dr. Gastón Pérez", medico: "indigorxdrperez" }] },
+    { nombre: "ELECTROENCEFALOGRAMA", links: [{ text: "Dr. Aldana Gómez", medico: "indigoeeggomez" }] },
+    { nombre: "VIDEOFIBROLARINGOSCOPIA", links: [{ text: "Dra. Paula Hernández", medico: "indigovflhernandez" }] },
+    { nombre: "ECOCARDIO DOPPLER COLOR", links: [{ text: "Dr. Maxi González", medico: "indigoecocardiogonzalez" }] }
+  ]
+};
