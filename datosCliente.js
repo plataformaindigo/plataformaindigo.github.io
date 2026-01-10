1ead81fd168df83d9c919c73bf6c83329e04d1ab
plataformaindigo <jorgemartingrassi@gmail.com>
2026-01-10 00:14:22 -0300

    Update datosCliente.js

--- datosCliente.js
+++ datosCliente.js
@@ -5,9 +5,9 @@
   nombre: "INDIGO",
   especialidades: "Simulación de Especialidades Médicas",
   descripcion: `
-    Clínica virtual con especialistas ficticios, creada con fines de prueba del sistema de emisión de informes médicos de la Plataforma INDIGO.<br><br>
-    Solicite su propia versión personalizada del sistema, utilizando su nombre institucional o profesional, identidad visual, membretes y estructura de informes médicos adaptada a sus prácticas.<br><br>
-    Plataforma INDIGO ofrece una prueba de 30 días, sin compromiso de suscripción, durante la cual podrá generar informes médicos con la estética y estructura de su institución o consultorio.
+    Clínica virtual con especialistas ficticios, creada con fines de prueba y demostración del sistema informes médicos de la Plataforma INDIGO.<br><br>
+       
+    Plataforma INDIGO ofrece una prueba de 30 días, sin compromiso de suscripción, durante la cual podrá generar informes médicos con la estética y estructura de su institución o consultorio aprovechando el 100% de sus funcionalidades.
   `,
   menu: [
     { nombre:"PSIQUIATRIA", links:[{ text:"Psi. Romina Vázquez", id:"indigopsivazquez" }]},
